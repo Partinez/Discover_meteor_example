2971a5f6cece48b7dde05d9790b900f6a50cdc53
emMartin Basterrechea/em <Partinez@users.noreply.github.com>
2015-09-23 14:48:50 +0200

    Commit chapter 7

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,8 +6,18 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+    Meteor.call('postInsert', post, function(error, result) {
 
-    post._id = Posts.insert(post);
-    Router.go('postPage',post);
+      //display the error to the user and abort
+      if (error) {
+        return alert(error.reason);
+      }
+
+      if (result.postExists) {
+        alert('This link has already been posted');
+      }
+      Router.go('postPage',{_id: result._id});
+    });
+
   }
-})
+});
